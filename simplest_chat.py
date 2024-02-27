3ef699fe9d483a87cbdcf9875257db8121b67652
Moon Lockwood <131006825+moonlockwood@users.noreply.github.com>
2024-02-27 05:59:55 +0000

    gitignore

--- simplest_chat.py
+++ simplest_chat.py
@@ -1,12 +1,13 @@
 from openai import OpenAI
 
-api_key = 'xxxxxxxxxx'
+with open("key.cfg", "r") as file:
+    api_key = file.read()
 
 client = OpenAI(api_key=api_key)
 
 model = "gpt-4-1106-preview"
 
-message = "What is a nice mellow colour?"
+message = "Respond with a paragraph suitable for a reading exercise for a person learning to read. "
 
 messages=   [
     {"role": "system", "content": "You are a helpful assistant."},
@@ -14,9 +15,9 @@ messages=   [
             ]
 
 response = client.chat.completions.create(
-  model=model,
-  messages=messages,
-  temperature=0.82
+    model=model,
+    messages=messages,
+    temperature=0.82
 )
 
 reply = response.choices[0]
